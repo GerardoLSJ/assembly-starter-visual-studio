4f4d4c14d50baf756919d65282aca31721b3c976
Gerardo LSJ <gerardo@GMacBook.local>
2017-09-10 17:39:28 -0500

    imul

--- ConsoleApplication1/ConsoleApplication1/int_parser.asm
+++ ConsoleApplication1/ConsoleApplication1/int_parser.asm
@@ -5,6 +5,7 @@
 string_input DB "12345",0h
 len equ $ - string_input
 result DB 00000000,0h
+temp DB 00000000,0h
 .CODE
 ;ebx : contadpr
                             ;EXAMPLE: -2 == 0xFFFF FFFE
@@ -20,15 +21,16 @@ for_loop:
 	mov cl, [eax + ebx]		;Tomamos el primer char porque [CX, CH, CL] 16 bits, 8 bits, 8 bits 
 
 	cmp  cl, '0' 			; Validate is a number in this intercal 
-	jb lessZero				; oterwise finish
+	jb lessZero				; oterwise finish JA, JB when comparing unsigned integers,
 	cmp  cl, '9'
 	ja greaterNine
 
 	sub cl, '0'				;ELSE is a number
 	;mov [edx + ebx], cl		; Guarda el "NUMERO" en la direccion de memoria indicada
 	movzx  cx , cl
-	imul cx, 10
-	add result, cx
+	mov temp, cl
+	imul temp, 10
+	add result, temp
 	dec ebx					; counter ++
 	
 	mov ch, [eax + ebx - 1]		;Vemos si sigue un 0
@@ -54,7 +56,7 @@ checkSign:
 positiveNumber:
 	; encender bandera de que es un numero positivo
 	xor ecx,ecx
-	dec ebx					;counter ++
+	dec ebx						;counter ++
 	;mov cl, [eax + ebx]		;Tomamos el siguiete valor porque fue signo
 	ret
 
