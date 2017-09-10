b44c0262d2b51feabfe410e5b0185bbc0bc70fc0
Gerardo Jimenez <Gerardo Jimenez>
2017-09-10 12:25:03 -0500

    int

--- ConsoleApplication1/ConsoleApplication1/int_parser.asm
+++ ConsoleApplication1/ConsoleApplication1/int_parser.asm
@@ -2,43 +2,63 @@
 .MODEL FLAT, C
 .STACK
 .DATA
-string_input DB "hello world",0
+string_input DB "10045",0h
 .CODE
                             ;EXAMPLE: -2 == 0xFFFF FFFE
 int_parser PROC
 	lea eax, string_input	;load effective adress of this.string
 	mov ebx, 0				;Contador init in zero
+	mov ecx, [eax + ebx]	;Para ver si es signo	
+	call checkSign			;Si no fue signo no AUMENTA el contador
+
+for_loop:
+	mov cl, [eax + ebx]		;Tomamos el primer char porque [CX, CH, CL] 16 bits, 8 bits, 8 bits 
+
+	cmp  cl, '0' 			; Validate is a number in this intercal 
+	jb lessZero				; oterwise finish
+	cmp  cl, '9'
+	ja greaterNine
+
+	sub cl, '0'				;ELSE is a number
+	mov [eax + ebx]h, cl		; Guarda el "NUMERO" en la direccion de memoria indicada 
+	inc ebx					; counter ++
 	
-for_loop:	
-	mov cl, [eax + ebx]		;Tomamos solo la "H" porque [CX CH CL] solo tomas el ultimo valor
-	cmp cl, 20h
-	je space				;Si "cl" vale 20 en HEX es un espacio, no lo incrementes
-	sub cl, 20h				;Restamos 20 a la "h"
-space:		
-	mov [eax + ebx], cl		; Guarda la "H" en la direccion de memoria indicada
-	inc ebx					;incrementamos el contadr
-	mov ch, [eax + ebx]
-	cmp ch,0				;compara si es 0 en esa localidad de memoria
+	mov ch, [eax + ebx]		;Vemos si sigue un 0
+	cmp ch, 0				;compara si es 0 en esa localidad de memoria
 	jne for_loop			;jump not equal
 	ret
 
-    mov edx, num3entered ; our string
-atoi:
-    xor eax, eax ; zero a "result so far"
-.top:
-    movzx ecx, byte [edx] ; get a character
-    inc edx ; ready for next one
-    cmp ecx, '0' ; valid?
-    jb .done
-    cmp ecx, '9'
-    ja .done
-    sub ecx, '0' ; "convert" character to number
-    imul eax, 10 ; multiply "result so far" by ten
-    add eax, ecx ; add in current digit
-    jmp .top ; until done
-.done:
-    ret
+lessZero:
+	xor ecx,ecx		;end 
+	jecxz done 
+	
+greaterNine:
+	xor ecx,ecx		;end
+	jecxz done
 
+checkSign:
+	cmp  cl, '+'
+	je positiveNumber
+	cmp  cl, '-'
+	je negativeNumber
+	ret
+
+positiveNumber:
+	; encender bandera de que es un numero positivo
+	xor ecx,ecx
+	inc ebx					;counter ++
+	;mov cl, [eax + ebx]		;Tomamos el siguiete valor porque fue signo
+	ret
+
+negativeNumber:
+	; encender bandera de que es un numero positivo
+	xor ecx,ecx
+	inc ebx					;counter ++
+	;mov cl, [eax + ebx]		;Tomamos el siguiete valor porque fue signo
+	ret
+
+done:
+ret
 int_parser ENDP
 
 END
