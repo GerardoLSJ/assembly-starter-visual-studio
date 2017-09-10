3aff3a2ad4ee6c230fcbf36bdff2d65fac3fef0d
Gerardo Jimenez <Gerardo Jimenez>
2017-09-10 17:27:38 -0500

    omg

--- ConsoleApplication1/ConsoleApplication1/int_parser.asm
+++ ConsoleApplication1/ConsoleApplication1/int_parser.asm
@@ -2,14 +2,19 @@
 .MODEL FLAT, C
 .STACK
 .DATA
-string_input DB "10045",0h
+string_input DB "12345",0h
+len equ $ - string_input
+result DB 00000000,0h
 .CODE
+;ebx : contadpr
                             ;EXAMPLE: -2 == 0xFFFF FFFE
 int_parser PROC
+	mov ecx, len 
 	lea eax, string_input	;load effective adress of this.string
-	mov ebx, 0				;Contador init in zero
-	mov ecx, [eax + ebx]	;Para ver si es signo	
-	call checkSign			;Si no fue signo no AUMENTA el contador
+	lea edx, result			;load effective adress of result
+	mov ebx, len-2			;Contador
+	;mov ecx, [eax + ebx]	;Para ver si es signo	
+	;call checkSign			;Checar el signo
 
 for_loop:
 	mov cl, [eax + ebx]		;Tomamos el primer char porque [CX, CH, CL] 16 bits, 8 bits, 8 bits 
@@ -20,10 +25,13 @@ for_loop:
 	ja greaterNine
 
 	sub cl, '0'				;ELSE is a number
-	mov [eax + ebx]h, cl		; Guarda el "NUMERO" en la direccion de memoria indicada 
-	inc ebx					; counter ++
+	;mov [edx + ebx], cl		; Guarda el "NUMERO" en la direccion de memoria indicada
+	movzx  cx , cl
+	imul cx, 10
+	add result, cx
+	dec ebx					; counter ++
 	
-	mov ch, [eax + ebx]		;Vemos si sigue un 0
+	mov ch, [eax + ebx - 1]		;Vemos si sigue un 0
 	cmp ch, 0				;compara si es 0 en esa localidad de memoria
 	jne for_loop			;jump not equal
 	ret
@@ -46,14 +54,14 @@ checkSign:
 positiveNumber:
 	; encender bandera de que es un numero positivo
 	xor ecx,ecx
-	inc ebx					;counter ++
+	dec ebx					;counter ++
 	;mov cl, [eax + ebx]		;Tomamos el siguiete valor porque fue signo
 	ret
 
 negativeNumber:
 	; encender bandera de que es un numero positivo
 	xor ecx,ecx
-	inc ebx					;counter ++
+	dec ebx					;counter ++
 	;mov cl, [eax + ebx]		;Tomamos el siguiete valor porque fue signo
 	ret
 
